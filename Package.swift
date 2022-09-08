2e9699fa2663e2ed43c1044ebe07cafd08e07735
momofinc <99238512+momofeggyu@users.noreply.github.com>
2022-09-08 17:03:09 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PhotoRoomKit-Swift",
+            path: "Sources",
+            sources: ["iOS"],
             dependencies: []),
         .testTarget(
             name: "PhotoRoomKit-SwiftTests",
